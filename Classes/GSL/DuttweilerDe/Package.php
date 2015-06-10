13164c5e30fa5b2d62061ce85041090c3a7b4cfb
PalatinCoder <github@jan-sl.de>
2015-06-10 18:39:43 +0200

    Revert "Remove GCM related code, as this feature was branched off"
    This reverts commit 8f870ce5d92e7d7ecb2f49bc9e50e7f8e6753f83.

---
+++ Classes/GSL/DuttweilerDe/Package.php
@@ -0,0 +1,19 @@
+<?php
+namespace GSL\DuttweilerDe;
+
+use TYPO3\Flow\Package\Package as BasePackage;
+use TYPO3\Flow\Core\Bootstrap;
+
+class Package extends BasePackage {
+
+	/**
+	 * Register slots for to get notified on new nodes, to push them to DuttweilerApp via GCM
+	 *
+	 * @param \TYPO3\Flow\Core\Bootstrap $bootstrap The current bootstrap
+	 * @return void
+	 */
+	public function boot(Bootstrap $bootstrap) {
+		$dispatcher = $bootstrap->getSignalSlotDispatcher();
+		$dispatcher->connect('TYPO3\Neos\Service\PublishingService', 'nodePublished', 'GSL\DuttweilerDe\Service\NotificationService', 'notifyNodePublished');
+	}
+}
---
+++ Classes/GSL/DuttweilerDe/Service/NotificationService.php
@@ -0,0 +1,48 @@
+<?php
+namespace GSL\DuttweilerDe\Service;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\Controller\ControllerInterface;
+use TYPO3\Flow\Mvc\RequestInterface;
+use TYPO3\Flow\Mvc\ResponseInterface;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\Flow\Http\Response;
+use TYPO3\Neos\Controller\Frontend\NodeController;
+use TYPO3\Flow\Log\SystemLoggerInterface;
+
+/**
+ * Service for sending notification via GCM when new nodes are published
+ *
+ * @Flow\Scope("singleton")
+ */
+
+class NotificationService {
+
+	/**
+	 * Check if the published node is a News and send GCM request
+	 *
+	 * @param NodeInterface $node
+	 * @return void
+	 */
+	
+	public function notifyNodePublished(NodeInterface $node) {	
+		
+		if (!$node->getNodeType()->isOfType('GSL.DuttweilerDe.Pages:ChronikItem')) {
+			return;
+		}
+		
+		$url = "http://localhost/not-existing-url" + $node->getName();
+
+		$ch = curl_init($url);	
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_REFERER, "News: " + $node->getName());
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		
+		curl_exec($ch);
+
+		curl_close($ch);
+	}
+}
+?>
