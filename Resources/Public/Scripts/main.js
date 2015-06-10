23ee0c29876e389c502220c21d6d13b3b3c3c5f8
PalatinCoder <github@jan-sl.de>
2015-06-10 11:09:41 +0200

    Add fancyboxGroup property to images

--- Resources/Public/Scripts/main.js
+++ Resources/Public/Scripts/main.js
@@ -1,4 +1,4 @@
-jQuery('.typo3-neos-nodetypes-image a.lightbox, .typo3-neos-nodetypes-textwithimage a.lightbox').attr('rel', 'gallery').fancybox({
+jQuery('.typo3-neos-nodetypes-image a.fancybox, .typo3-neos-nodetypes-textwithimage a.fancybox').fancybox({
 	helpers: {
 		title: {
 			type: 'inside'
