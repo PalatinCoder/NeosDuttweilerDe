39f0aa39d2f32c4a20da3b0c7f56b22ffc2e3656
PalatinCoder <github@jan-sl.de>
2015-12-26 14:08:12 +0100

    add back-to-top button

--- Resources/Public/Scripts/main.js
+++ Resources/Public/Scripts/main.js
@@ -21,3 +21,23 @@ jQuery(".fancybox-various").fancybox({
 		}
 	}
 });
+
+jQuery(document).ready(function() {
+	var back_to_top_button = ['<a href="#top" class="back-to-top">Nach oben</a>'].join("");
+	$('body').append(back_to_top_button);
+	$('.back-to-top').hide();
+
+	$(function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 200) {
+				$('.back-to-top').fadeIn();
+			} else {
+				$('.back-to-top').fadeOut();
+			}
+		});
+		$('.back-to-top').click(function() {
+			$('body, html').animate({ scrollTop: 0 }, 800);
+			return false;
+		});
+	});
+});
