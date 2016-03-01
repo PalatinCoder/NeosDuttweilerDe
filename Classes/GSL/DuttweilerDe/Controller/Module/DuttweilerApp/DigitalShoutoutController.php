91271238ffdc71c7d17e272dba79ae59d1baafa3
PalatinCoder <github@jan-sl.de>
2016-03-01 14:35:56 +0100

    add dummy DigitalShoutoutController

---
+++ Classes/GSL/DuttweilerDe/Controller/Module/DuttweilerApp/DigitalShoutoutController.php
@@ -0,0 +1,20 @@
+<?php
+namespace GSL\DuttweilerDe\Controller\Module\DuttweilerApp;
+
+/**
+ * This file is part of the GSL.DuttweilerDe Neos Site Package
+ *
+ * @author Jan Syring-Lingenfelder
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Neos\Controller\Module\AbstractModuleController;
+
+/**
+ * The DigitalShoutout Controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class DigitalShoutoutController extends AbstractModuleController {
+
+}
