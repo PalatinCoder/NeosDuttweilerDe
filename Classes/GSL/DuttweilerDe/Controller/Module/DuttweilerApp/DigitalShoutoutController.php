d9216a7b4d5d972585cbe646c01813e28525c9d1
PalatinCoder <github@jan-sl.de>
2016-03-01 15:52:01 +0100

    implement DigitalShoutoutController
    for now, the controller only reads back the sent data

--- Classes/GSL/DuttweilerDe/Controller/Module/DuttweilerApp/DigitalShoutoutController.php
+++ Classes/GSL/DuttweilerDe/Controller/Module/DuttweilerApp/DigitalShoutoutController.php
@@ -8,6 +8,7 @@ namespace GSL\DuttweilerDe\Controller\Module\DuttweilerApp;
  */
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Error\Message;
 use TYPO3\Neos\Controller\Module\AbstractModuleController;
 
 /**
@@ -17,4 +18,41 @@ use TYPO3\Neos\Controller\Module\AbstractModuleController;
  */
 class DigitalShoutoutController extends AbstractModuleController {
 
+	/**
+	 * @return void
+	 */
+	protected function initializeAction() {
+		parent::initializeAction();
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function initializeView(\TYPO3\Flow\Mvc\View\ViewInterface $view) {
+		parent::initializeView($view);
+		$view->setLayoutPathAndFilename('../Packages/Application/TYPO3.Neos/Resources/Private/Layouts/BackendModule.html');
+		$view->setPartialRootPath('../Packages/Application/TYPO3.Neos/Resources/Private/Partials');
+	}
+
+	/**
+	 * Show a form to enter a message
+	 * 
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+
+    /**
+     * Send a message to GCM
+     *
+     * @param string $heading The Heading
+     * @param string $message The Message
+     * @Flow\Validate(argumentName="heading", type="\TYPO3\Flow\Validation\Validator\NotEmptyValidator")
+     * @return void
+     */
+    public function sendAction($heading, $message) {
+		$this->addFlashMessage('Die Nachricht "%s" mit dem Inhalt "%s" wurde gesendet.', 'Durchsage gesendet', Message::SEVERITY_OK, array(htmlspecialchars($heading), htmlspecialchars($message)), 1456841323);
+		$this->redirect('index');
+    }
 }
+?>
