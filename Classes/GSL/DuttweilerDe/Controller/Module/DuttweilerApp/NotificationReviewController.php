ff45ef98a874b2520a55c4a10ad2c2c3879fe795
PalatinCoder <github@jan-sl.de>
2016-03-03 15:53:16 +0100

    implement dummy controller for notification review

---
+++ Classes/GSL/DuttweilerDe/Controller/Module/DuttweilerApp/NotificationReviewController.php
@@ -0,0 +1,88 @@
+<?php
+namespace GSL\DuttweilerDe\Controller\Module\DuttweilerApp;
+
+/**
+ * This file is part of the GSL.DuttweilerDe Neos Site Package
+ *
+ * @author Jan Syring-Lingenfelder
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Error\Message;
+use TYPO3\Neos\Controller\Module\AbstractModuleController;
+use \GSL\DuttweilerDe\Service\GcmHelper;
+
+/**
+ * The NotificationManagement Controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class NotificationReviewController extends AbstractModuleController {
+
+	/**
+	 * @return void
+	 */
+	protected function initializeAction() {
+		parent::initializeAction();
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function initializeView(\TYPO3\Flow\Mvc\View\ViewInterface $view) {
+		parent::initializeView($view);
+		$view->setLayoutPathAndFilename('../Packages/Application/TYPO3.Neos/Resources/Private/Layouts/BackendModule.html');
+		$view->setPartialRootPath('../Packages/Application/TYPO3.Neos/Resources/Private/Partials');
+	}
+
+	/**
+	 * Show a list of all pending push notifications
+	 * 
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+
+	/**
+	 * Send a single notification
+	 *
+	 * @param int $notificationId Identifier of the notification
+	 * @return void
+	 */
+	public function sendAction($notificationId) {
+		$this->addFlashMessage('Dummy: send %d', 'Dummy Impelementation', Message::SEVERITY_OK, array($notificationId));
+		$this->redirect('index');
+	}
+
+	/**
+	 * Discard a single notification
+	 *
+	 * @param int $notificationId The identifier of the notification
+	 * @return void
+	 */
+	public function discardAction($notificationId) {
+		$this->addFlashMessage('Dummy: discard %d', 'Dummy Impelementation', Message::SEVERITY_OK, array($notificationId));
+		$this->redirect('index');
+	}
+
+	/**
+	 * Send all notifications
+	 *
+	 * @return void
+	 */
+	public function sendAllAction() {
+		$this->addFlashMessage('Dummy: sendAll', 'Dummy Impelementation');
+		$this->redirect('index');
+	}
+
+	/**
+	 * Discard all notifications
+	 *
+	 * @return void
+	 */
+	public function discardAllAction() {
+		$this->addFlashMessage('Dummy: discardAll', 'Dummy Impelementation');
+		$this->redirect('index');
+	}
+}
+?>
