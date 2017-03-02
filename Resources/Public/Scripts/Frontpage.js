dfdbcdbdd92295190181ec74128758b21544edda
PalatinCoder <github@jan-sl.de>
2017-03-02 14:02:01 +0100

    FEATURE: CSS animted hamburger button

--- Resources/Public/Scripts/Frontpage.js
+++ Resources/Public/Scripts/Frontpage.js
@@ -1,6 +1,7 @@
 $(function() {
     $('#menuButton').click(function() {
       $('#IndexPageTopMenu').toggleClass('is-open');
+      $('#menuButton').toggleClass('is-open');
     });
     $('#NewsColumnResponsiveHeading').click(function() {
       $('#NewsColumn').toggleClass('is-closed');
