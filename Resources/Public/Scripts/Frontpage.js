023a6f1434848c11544691b7e305dd9324554a9c
PalatinCoder <github@jan-sl.de>
2017-03-02 13:20:18 +0100

    TASK: Use css animations for the responsiveness
    Get rid of some javascript that was pretty messy

---
+++ Resources/Public/Scripts/Frontpage.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#menuButton').click(function() {
+      $('#IndexPageTopMenu').toggleClass('is-open');
+    });
+    $('#NewsColumnResponsiveHeading').click(function() {
+      $('#NewsColumn').toggleClass('is-closed');
+      $('#NewsColumnResponsiveHeading').toggleClass('is-closed');
+    });
+    $.smartbanner({
+      title: 'DuttweilerApp',
+      author: 'Jan Syring-Lingenfelder',
+      button: 'Installieren',
+inGooglePlay: 'bei Google Play',
+icon: 'http://static.duttweiler-net.de/wappen/wappen8.gif',
+      price: 'Kostenlos'
+    });
+});
